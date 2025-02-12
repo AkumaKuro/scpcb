e8d6271424e8f986e4fa85fe92ab62c481523ea9
AkumaKuro <p.ferreiraherda@gmail.com>
2025-02-12 14:25:44 -0500

    Translate Achievements to GDScript

--- Achievements.gd
+++ Achievements.gd
@@ -1,181 +1,166 @@
-;achievement menu & messages by InnocentSam
+extends Node
 
-Const MAXACHIEVEMENTS=37
-Dim Achievements%(MAXACHIEVEMENTS)
+const MAXACHIEVEMENTS: int = 37
 
-Const Achv008%=0, Achv012%=1, Achv035%=2, Achv049%=3, Achv055=4,  Achv079%=5, Achv096%=6, Achv106%=7, Achv148%=8, Achv205=9
-Const Achv294%=10, Achv372%=11, Achv420%=12, Achv427=13, Achv500%=14, Achv513%=15, Achv714%=16, Achv789%=17, Achv860%=18, Achv895%=19
-Const Achv914%=20, Achv939%=21, Achv966%=22, Achv970=23, Achv1025%=24, Achv1048=25, Achv1123=26
+const Achv008: int = 0
+const Achv012: int = 1
+const Achv035: int = 2
+const Achv049: int = 3
+const Achv055: int = 4
+const Achv079: int = 5
+const Achv096: int = 6
+const Achv106: int = 7
+const Achv148: int = 8
+const Achv205: int = 9
+const Achv294: int = 10
+const Achv372: int = 11
+const Achv420: int = 12
+const Achv427: int = 13
+const Achv500: int = 14
+const Achv513: int = 15
+const Achv714: int = 16
+const Achv789: int = 17
+const Achv860: int = 18
+const Achv895: int = 19
+const Achv914: int = 20
+const Achv939: int = 21
+const Achv966: int = 22
+const Achv970: int = 23
+const Achv1025: int = 24
+const Achv1048: int = 25
+const Achv1123: int = 26
 
-Const AchvMaynard%=27, AchvHarp%=28, AchvSNAV%=29, AchvOmni%=30, AchvConsole%=31, AchvTesla%=32, AchvPD%=33
+const AchvMaynard: int = 27
+const AchvHarp: int = 28
+const AchvSNAV: int = 29
+const AchvOmni: int = 30
+const AchvConsole: int = 31
+const AchvTesla: int = 32
+const AchvPD: int = 33
 
-Const Achv1162% = 34, Achv1499% = 35
+const Achv1162: int = 34
+const Achv1499: int = 35
 
-Const AchvKeter% = 36
+const AchvKeter: int = 36
 
-Global UsedConsole
+func _ready() -> void:
+	for i: int in range(MAXACHIEVEMENTS):
+		var loc2: int = GetINISectionLocation("Data/achievementstrings.ini", "s"+Str(i))
+		AchievementStrings[i] = GetINIString2("Data/achievementstrings.ini", loc2, "string1")
+		AchievementDescs[i] = GetINIString2("Data/achievementstrings.ini", loc2, "AchvDesc")
 
-Global AchievementsMenu%
-Global AchvMSGenabled% = GetINIInt("options.ini", "options", "achievement popup enabled")
-Dim AchievementStrings$(MAXACHIEVEMENTS)
-Dim AchievementDescs$(MAXACHIEVEMENTS)
-Dim AchvIMG%(MAXACHIEVEMENTS)
-For i = 0 To MAXACHIEVEMENTS-1
-	Local loc2% = GetINISectionLocation("Data\achievementstrings.ini", "s"+Str(i))
-	AchievementStrings(i) = GetINIString2("Data\achievementstrings.ini", loc2, "string1")
-	AchievementDescs(i) = GetINIString2("Data\achievementstrings.ini", loc2, "AchvDesc")
-	
-	Local image$ = GetINIString2("Data\achievementstrings.ini", loc2, "image") 
-	
-	AchvIMG(i) = LoadImage_Strict("GFX\menu\achievements\"+image+".jpg")
-	AchvIMG(i) = ResizeImage2(AchvIMG(i),ImageWidth(AchvIMG(i))*GraphicHeight/768.0,ImageHeight(AchvIMG(i))*GraphicHeight/768.0)
-Next
+		var image: String = GetINIString2("Data/achievementstrings.ini", loc2, "image")
 
-Global AchvLocked = LoadImage_Strict("GFX\menu\achievements\achvlocked.jpg")
-AchvLocked = ResizeImage2(AchvLocked,ImageWidth(AchvLocked)*GraphicHeight/768.0,ImageHeight(AchvLocked)*GraphicHeight/768.0)
+		AchvIMG[i] = LoadImage_Strict("GFX/menu/achievements/"+image+".jpg")
+		AchvIMG[i] = ResizeImage2(AchvIMG[i],ImageWidth(AchvIMG[i])*GraphicHeight/768.0,ImageHeight(AchvIMG[i])*GraphicHeight/768.0)
 
-Function GiveAchievement(achvname%, showMessage%=True)
-	If Achievements(achvname)<>True Then
-		Achievements(achvname)=True
-		If AchvMSGenabled And showMessage Then
-			Local loc2% = GetINISectionLocation("Data\achievementstrings.ini", "s"+achvname)
-			Local AchievementName$ = GetINIString2("Data\achievementstrings.ini", loc2, "string1")
-			;Msg = "Achievement Unlocked - "+AchievementName
-			;MsgTimer=70*7
+	AchvLocked = ResizeImage2(AchvLocked,ImageWidth(AchvLocked)*GraphicHeight/768.0,ImageHeight(AchvLocked)*GraphicHeight/768.0)
+
+func GiveAchievement(achvname: int, showMessage: int=True) -> void:
+	if !Achievements[achvname]:
+		Achievements[achvname]=True
+		if AchvMSGenabled and showMessage:
+			var loc2: int = GetINISectionLocation("Data/achievementstrings.ini", "s"+achvname)
+			var AchievementName: String = GetINIString2("Data/achievementstrings.ini", loc2, "string1")
 			CreateAchievementMsg(achvname,AchievementName)
-		EndIf
-	EndIf
-End Function
 
-Function AchievementTooltip(achvno%)
-    Local scale# = GraphicHeight/768.0
-    
-    AASetFont Font3
-    Local width = AAStringWidth(AchievementStrings(achvno))
-    AASetFont Font1
-    If (AAStringWidth(AchievementDescs(achvno))>width) Then
-        width = AAStringWidth(AchievementDescs(achvno))
-    EndIf
-    width = width+20*MenuScale
-    
-    Local height = 38*scale
-    
-    Color 25,25,25
-    Rect(ScaledMouseX()+(20*MenuScale),ScaledMouseY()+(20*MenuScale),width,height,True)
-    Color 150,150,150
-    Rect(ScaledMouseX()+(20*MenuScale),ScaledMouseY()+(20*MenuScale),width,height,False)
-    AASetFont Font3
-    AAText(ScaledMouseX()+(20*MenuScale)+(width/2),ScaledMouseY()+(35*MenuScale), AchievementStrings(achvno), True, True)
-    AASetFont Font1
-    AAText(ScaledMouseX()+(20*MenuScale)+(width/2),ScaledMouseY()+(55*MenuScale), AchievementDescs(achvno), True, True)
-End Function
+func AchievementTooltip(achvno: int) -> void:
+	var scale: float = GraphicHeight/768.0
 
-Function DrawAchvIMG(x%, y%, achvno%)
-	Local row%
-	Local scale# = GraphicHeight/768.0
-	Local SeparationConst2 = 76 * scale
-;	If achvno >= 0 And achvno < 4 Then 
-;		row = achvno
-;	ElseIf achvno >= 3 And achvno <= 6 Then
-;		row = achvno-3
-;	ElseIf achvno >= 7 And achvno <= 10 Then
-;		row = achvno-7
-;	ElseIf achvno >= 11 And achvno <= 14 Then
-;		row = achvno-11
-;	ElseIf achvno >= 15 And achvno <= 18 Then
-;		row = achvno-15
-;	ElseIf achvno >= 19 And achvno <= 22 Then
-;		row = achvno-19
-;	ElseIf achvno >= 24 And achvno <= 26 Then
-;		row = achvno-24
-;	EndIf
-	row = achvno Mod 4
-	Color 0,0,0
+	AASetFont(Font3)
+	var width = AAStringWidth(AchievementStrings(achvno))
+	AASetFont(Font1)
+	if (AAStringWidth(AchievementDescs(achvno))>width):
+		width = AAStringWidth(AchievementDescs(achvno))
+
+	width = width+20*MenuScale
+
+	var height = 38*scale
+
+	Color(25,25,25)
+	Rect(ScaledMouseX()+(20*MenuScale),ScaledMouseY()+(20*MenuScale),width,height,True)
+	Color(150,150,150)
+	Rect(ScaledMouseX()+(20*MenuScale),ScaledMouseY()+(20*MenuScale),width,height,False)
+	AASetFont(Font3)
+	AAText(ScaledMouseX()+(20*MenuScale)+(width/2),ScaledMouseY()+(35*MenuScale), AchievementStrings(achvno), True, True)
+	AASetFont(Font1)
+	AAText(ScaledMouseX()+(20*MenuScale)+(width/2),ScaledMouseY()+(55*MenuScale), AchievementDescs(achvno), True, True)
+
+func DrawAchvIMG(x: int, y: int, achvno: int) -> void:
+	var row: int
+	var scale: float = GraphicHeight/768.0
+	var SeparationConst2 = 76 * scale
+
+	row = achvno % 4
+	Color(0,0,0)
 	Rect((x+((row)*SeparationConst2)), y, 64*scale, 64*scale, True)
-	If Achievements(achvno) = True Then
+	if Achievements(achvno):
 		DrawImage(AchvIMG(achvno),(x+(row*SeparationConst2)),y)
-	Else
+	else:
 		DrawImage(AchvLocked,(x+(row*SeparationConst2)),y)
-	EndIf
-	Color 50,50,50
-	
+
+	Color(50,50,50)
+
 	Rect((x+(row*SeparationConst2)), y, 64*scale, 64*scale, False)
-End Function
 
-Global CurrAchvMSGID% = 0
 
-Type AchievementMsg
-	Field achvID%
-	Field txt$
-	Field msgx#
-	Field msgtime#
-	Field msgID%
-End Type
 
-Function CreateAchievementMsg.AchievementMsg(id%,txt$)
-	Local amsg.AchievementMsg = New AchievementMsg
-	
-	amsg\achvID = id
-	amsg\txt = txt
-	amsg\msgx = 0.0
-	amsg\msgtime = FPSfactor2
-	amsg\msgID = CurrAchvMSGID
-	CurrAchvMSGID = CurrAchvMSGID + 1
-	
-	Return amsg
-End Function
+class AchievementMsg:
+	var achvID: int
+	var txt: String
+	var msgx: float
+	var msgtime: float
+	var msgID: int
 
-Function UpdateAchievementMsg()
-	Local amsg.AchievementMsg,amsg2.AchievementMsg
-	Local scale# = GraphicHeight/768.0
-	Local width% = 264*scale
-	Local height% = 84*scale
-	Local x%,y%
-	
-	For amsg = Each AchievementMsg
-		If amsg\msgtime <> 0
-			x=GraphicWidth+amsg\msgx
+func CreateAchievementMsg(id: int,txt: String) -> AchievementMsg:
+	var amsg: AchievementMsg = AchievementMsg.new()
+
+	amsg.achvID = id
+	amsg.txt = txt
+	amsg.msgx = 0.0
+	amsg.msgtime = FPSfactor2
+	amsg.msgID = CurrAchvMSGID
+	CurrAchvMSGID += 1
+
+	return amsg
+
+func UpdateAchievementMsg() -> void:
+	var scale: float = GraphicHeight/768.0
+	var width: int = 264*scale
+	var height: int = 84*scale
+	var x: int
+	var y: int
+
+	for amsg: AchievementMsg in EachAchievementMsg:
+		if amsg.msgtime != 0:
+			x=GraphicWidth+amsg.msgx
 			y=(GraphicHeight-height)
-			For amsg2 = Each AchievementMsg
-				If amsg2 <> amsg
-					If amsg2\msgID > amsg\msgID
+			for amsg2: AchievementMsg in EachAchievementMsg:
+				if amsg2 != amsg:
+					if amsg2.msgID > amsg.msgID:
 						y=y-height
-					EndIf
-				EndIf
-			Next
+
 			DrawFrame(x,y,width,height)
-			Color 0,0,0
+			Color(0,0,0)
 			Rect(x+10*scale,y+10*scale,64*scale,64*scale,True)
-			DrawImage(AchvIMG(amsg\achvID),x+10*scale,y+10*scale)
-			Color 50,50,50
+			DrawImage(AchvIMG(amsg.achvID),x+10*scale,y+10*scale)
+			Color(50,50,50)
 			Rect(x+10*scale,y+10*scale,64*scale,64*scale,False)
-			Color 255,255,255
-			AASetFont Font1
-			RowText("Achievement Unlocked - "+amsg\txt,x+84*scale,y+10*scale,width-94*scale,y-20*scale)
-			If amsg\msgtime > 0.0 And amsg\msgtime < 70*7
-				amsg\msgtime = amsg\msgtime + FPSfactor2
-				If amsg\msgx > -width%
-					amsg\msgx = Max(amsg\msgx-4*FPSfactor2,-width%)
-				EndIf
-			ElseIf amsg\msgtime >= 70*7
-				amsg\msgtime = -1
-			ElseIf amsg\msgtime = -1
-				If amsg\msgx < 0.0
-					amsg\msgx = Min(amsg\msgx+4*FPSfactor2,0.0)
-				Else
-					amsg\msgtime = 0.0
-				EndIf
-			EndIf
-		Else
-			Delete amsg
-		EndIf
-	Next
-	
-End Function
+			Color(255,255,255)
+			AASetFont(Font1)
+			RowText("Achievement Unlocked - "+amsg.txt,x+84*scale,y+10*scale,width-94*scale,y-20*scale)
+			if amsg.msgtime > 0.0 and amsg.msgtime < 70*7:
+				amsg.msgtime = amsg.msgtime + FPSfactor2
+				if amsg.msgx > -width:
+					amsg.msgx = Max(amsg.msgx-4*FPSfactor2,-width)
 
+			elif amsg.msgtime >= 70*7:
+				amsg.msgtime = -1
+			elif amsg.msgtime == -1:
+				if amsg.msgx < 0.0:
+					amsg.msgx = Min(amsg.msgx+4*FPSfactor2,0.0)
+				else:
+					amsg.msgtime = 0.0
 
-
-
-;~IDEal Editor Parameters:
-;~F#31#48
-;~C#Blitz3D
+		else:
+			Delete(amsg)
